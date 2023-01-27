0e8a8ded671c39e9bbc0ac6c91809096bad870e8
JosiahFu <josiahfu@gmail.com>
2023-01-27 10:39:56 -0800

    Added MathNoteField & CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import { addStyles, MathField, EditableMathField } from '@numberworks/react-mathquill'
 import './App.css';
 
+addStyles();
+
+enum FieldType {
+    Math = 'MATH',
+    Text = 'TEXT'
+}
+
+class MathNoteField extends React.Component {
+    state: {
+        type: FieldType, initialValue: string
+    }
+
+    constructor(props: { value: string }) {
+        super(props);
+        this.state = { type: FieldType.Math, initialValue: props.value ?? '' }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(field: MathField): void {
+        if (field.latex() === '"') {
+            this.setState({ type: FieldType.Text });
+        }
+    }
+
+    render() {
+        return (
+            this.state.type === FieldType.Math ?
+                <EditableMathField latex={this.state.initialValue} onChange={this.handleChange} config={{spaceBehavesLikeTab: true}} />
+                : <input autoFocus className="text-note" />
+        )
+    }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <MathNoteField />
+            <MathNoteField />
+            <MathNoteField />
+            <MathNoteField />
+            <MathNoteField />
+            <MathNoteField />
+            <MathNoteField />
+        </div>
+    );
 }
 
 export default App;
