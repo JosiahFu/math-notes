dcdc75908d47fc2a300db5926036cddd7701533f
JosiahFu <josiahfu@gmail.com>
2023-01-29 09:22:08 -0800

    Formatting, styles, and title

--- src/App.tsx
+++ src/App.tsx
@@ -51,36 +51,58 @@ function MathNoteField(props: MathNoteFieldProps) {
 
     return (
         props.type === FieldType.Math ?
-        <EditableMathField
-            latex={props.value}
-            onChange={handleMathFieldChange}
-            config={{ spaceBehavesLikeTab: true }}
-            onFocus={props.onFocus}
-            mathquillDidMount={target => mathField.current = target}
-        /> :
-        <input
-            className="text-note"
-            onChange={handleTextNoteChange}
-            value={props.value}
-            onFocus={props.onFocus}
-            ref={textInput}
-        />
+            <EditableMathField
+                latex={props.value}
+                onChange={handleMathFieldChange}
+                config={{ spaceBehavesLikeTab: true }}
+                onFocus={props.onFocus}
+                mathquillDidMount={target => mathField.current = target}
+            /> :
+            <input
+                className="text-note"
+                onChange={handleTextNoteChange}
+                value={props.value}
+                onFocus={props.onFocus}
+                ref={textInput}
+            />
     );
 }
 
-interface TitleProps { value: string, setValue: (value: string) => void }
+interface TitleProps { value: string, setValue: (value: string) => void, defaultValue: string, onInput: (value: string) => void }
 function Title(props: TitleProps) {
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         props.setValue(event.target.value);
-        document.title = event.target.value || 'Math Notes'; // If blank
     };
+    
+    const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+        fixTitle();
+        props.onInput(event.target.value);
+    }
+    
+    const fixTitle = () => {
+        if (props.value === '') {
+            props.setValue(props.defaultValue);
+        }
+    }
+
+    // Title resetting should ONLY run during initial mount or when user unfocuses
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(fixTitle, []);
 
     return (
-        <h1><input type="text" value={props.value} onChange={handleChange}/></h1>
+        <h1>
+            <input
+                type="text"
+                value={props.value} // If blank
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={props.value === props.defaultValue ? 'untitled' : ''}
+            />
+        </h1>
     )
 }
 
-interface NotesProps {lines: MathNoteState[], setLines: (lines: MathNoteState[]) => void}
+interface NotesProps { lines: MathNoteState[], setLines: (lines: MathNoteState[]) => void }
 function Notes(props: NotesProps) {
     const lines = props.lines;
     const setLines = props.setLines;
@@ -106,7 +128,7 @@ function Notes(props: NotesProps) {
                 break;
             case "Backspace":
                 if (lines[focusIndex].value === '' && lines.length > 1) {
-                    setLines(lines.filter((e,i) => i !== focusIndex))
+                    setLines(lines.filter((e, i) => i !== focusIndex))
                     focusIndex !== 0 && setFocusIndex(focusIndex - 1);
                     break; // Only capture event if something happens
                 }
@@ -119,7 +141,7 @@ function Notes(props: NotesProps) {
 
     return (
         <div className="notes" onKeyDown={handleKeyDown} ref={element}>
-            {lines.map((e, i) => 
+            {lines.map((e, i) =>
                 <MathNoteField
                     key={i}
                     value={e.value}
@@ -134,13 +156,17 @@ function Notes(props: NotesProps) {
 
 function App() {
     const [lines, setLines] = useState([{ value: '', type: FieldType.Math }] as { value: string, type: FieldType }[]);
-    const [title, setTitle] = useState('Untitled Notes');
+    const [title, setTitle] = useState('');
+    
+    const setDocumentTitle = (documentTitle: string) => {
+        document.title = title || 'Math Notes'; // If blank
+    }
 
     return (
-        <>
-            <Title value={title} setValue={setTitle} />
+        <main className="app">
+            <Title value={title} setValue={setTitle} defaultValue="Untitled Notes" onInput={setDocumentTitle} />
             <Notes lines={lines} setLines={setLines} />
-        </>
+        </main>
     );
 
 }
