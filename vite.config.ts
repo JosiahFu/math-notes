e926f624555f1cca4996511ba8de8c948a007e5e
JosiahFu <josiahfu@gmail.com>
2023-10-08 08:13:10 -0700

    build to single file

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
+    viteSingleFile(),
   ],
   base: '/math-notes',
 })
