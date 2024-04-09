07f16c2341ad7e27dc1d550b4f12ff6ebf7c07b2
JosiahFu <josiahfu@gmail.com>
2024-04-08 20:36:48 -0700

    Update build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
+const nonHashFiles = [
+    'woff',
+    'woff2',
+    'svg',
+    'ttf',
+    'eot',
+]
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -9,4 +17,15 @@ export default defineConfig({
     svgr(),
   ],
   base: '/math-notes',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: assetInfo => {
+          if (nonHashFiles.some(ext => assetInfo.name.endsWith(`.${ext}`)))
+            return 'assets/[name].[ext]'
+          return 'assets/[name]-[hash].[ext]'
+        }
+      }
+    }
+  }
 })
